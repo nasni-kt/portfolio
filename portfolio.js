08072559871f2c0d78b7316fd948b3ec66bac1ef
nasni <nasni964@gmail.com>
2024-06-13 22:00:31 +0530

    your commit name

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,26 @@
 function sendmail(){
     var params={
-        name:document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        message:document.getElementById("message").value,
-        
-        
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
     };
-    const serviceId="service_wxbyaw7"
-    const templateId="template_ljnunmm"
+    const serviceId = "service_wxbyaw7";
+    const templateId = "template_ljnunmm";
 
-    emailjs.send(serviceId,templateId,params)
-    .then(
-        res=>{
-            document.getElementById("name").value=""
-            document.getElementById("email").value=""
-            document.getElementById("message").value=""
+    emailjs.send(serviceId, templateId, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
             console.log(res);
-            alert("you message send successfully")
-
+            alert("Your message was sent successfully.");
         })
-    .catch((err)=>log(err))
+        .catch(err => console.log(err));
 }
+
+//     emailjs.send("service_kee3oeq","template_8h5y7lb",params).then(alert("email send!!"))
+
+
 
 
 
