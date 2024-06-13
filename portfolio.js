ae607491cbd1f6cde1608ae7e273350177038cba
nasni <nasni964@gmail.com>
2024-06-13 23:08:51 +0530

    Your descriptive commit message

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,25 @@
-function sendmail(){
+function sendMail(){
     var params={
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
+        name:document.getElementById("name").value ,
+        number:document.getElementById("number").value ,
+        email:document.getElementById("email").value ,
+        message:document.getElementById("message").value
     };
-    const serviceId = "service_kee3oeq";
-    const templateId = "template_8h5y7lb";
+    const serviceID="service_qdn8ufd";
+const templateID="template_yhwxr5a";
 
-    emailjs.send(serviceId, templateId, params)
-        .then(res => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Your message was sent successfully.");
-        })
-        .catch(err => console.log(err));
+emailjs.send(serviceID,templateID,params)
+.then(
+    res=>{
+        document.getElementById("name").value="";  
+        document.getElementById("number").value="";      
+        document.getElementById("email").value="";
+        document.getElementById("message").value="";
+        console.log(res);
+        alert("Your message send successfully");
+    })
+    .catch(err=console.log(err));
 }
-
 //     emailjs.send("service_kee3oeq","template_8h5y7lb",params).then(alert("email send!!"))
 
 
