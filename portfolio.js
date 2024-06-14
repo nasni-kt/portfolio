8bb83396cc703681377215d0c835109c2a119ef0
nasni <nasni964@gmail.com>
2024-06-14 09:43:52 +0530

    Your commit message

--- portfolio.js
+++ portfolio.js
@@ -5,24 +5,25 @@ function sendMail(){
         email:document.getElementById("email").value ,
         message:document.getElementById("message").value
     };
-    const serviceID="service_qdn8ufd";
-const templateID="template_yhwxr5a";
-
-emailjs.send(serviceID,templateID,params)
-.then(
-    res=>{
-        document.getElementById("name").value="";  
-        document.getElementById("number").value="";      
-        document.getElementById("email").value="";
-        document.getElementById("message").value="";
-        console.log(res);
-        alert("Your message send successfully");
-    })
-    .catch(err=console.log(err));
+ 
+    emailjs.send("service_kee3oeq","template_8h5y7lb",params).then(alert("email send!!"))
 }
-//     emailjs.send("service_kee3oeq","template_8h5y7lb",params).then(alert("email send!!"))
 
 
 
 
+// const serviceID="service_qdn8ufd";
+// const templateID="template_yhwxr5a";
 
+// emailjs.send(serviceID,templateID,params)
+// .then(
+//     res=>{
+//         document.getElementById("name").value="";  
+//         document.getElementById("number").value="";      
+//         document.getElementById("email").value="";
+//         document.getElementById("message").value="";
+//         console.log(res);
+//         alert("Your message send successfully");
+//     })
+//     .catch(err=console.log(err));
+// }
